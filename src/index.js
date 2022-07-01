2612415dc638f7748785b3a32545e3b0b988f311
OlenaKut <cundrochka@gmail.com>
2022-07-01 17:20:36 +0200

    Weather icon

--- src/index.js
+++ src/index.js
@@ -22,23 +22,30 @@ function formatDate(tamestamp) {
 }
 
 function dispayTemperature(response) {
-  console.log(response);
+  console.log(response.data);
+  let dateElement = document.querySelector("#date");
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#currentTemperature");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "82914429dbcac50d79e072b0c73a8cb3";
-let city = "london";
+let city = "rivne";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(dispayTemperature);
