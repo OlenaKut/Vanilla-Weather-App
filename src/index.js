68a0b2ea320c8dd79972061a294636cc41c485e6
OlenaKut <cundrochka@gmail.com>
2022-07-01 16:34:07 +0200

    Added API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+function formatDate(tamestamp) {
+  let date = new Date(tamestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursdy",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function dispayTemperature(response) {
   console.log(response);
   let cityElement = document.querySelector("#city");
@@ -5,6 +28,8 @@ function dispayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -13,6 +38,7 @@ function dispayTemperature(response) {
 }
 
 let apiKey = "82914429dbcac50d79e072b0c73a8cb3";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+let city = "london";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(dispayTemperature);
